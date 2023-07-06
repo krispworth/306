63832e0a328deda6d29bb33ef0a57de88354833c
krispworth <138101787+krispworth@users.noreply.github.com>
2023-07-06 15:00:46 +0800

    Add files via upload

---
+++ LABORATORY2.py
@@ -0,0 +1,14 @@
+num1 = 5
+num2 = 8
+
+sum = num1 + num2;
+print("SUM = {0}".format(sum))
+
+diff = num1 - num2;
+print("DIFFERENCE = {0}".format(diff))
+
+product = num1 * num2;
+print("PRODUCT = {0}".format(product))
+
+quotient = num1 / num2;
+print("QUOTIENT = {0}".format(quotient))
