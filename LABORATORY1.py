50657341738fcd99cabb96f422bcc48a496be600
krispworth <138101787+krispworth@users.noreply.github.com>
2023-07-06 15:06:59 +0800

    Update LABORATORY1.py

--- LABORATORY1.py
+++ LABORATORY1.py
@@ -1,5 +1,23 @@
-num1 = 5
-num2 = 8
+print("")
+print("")
 
-sum = num1 + num 2
-print("SUM = {0}".format(sum))
+print("Programmer: Ma. Mikcaela Ann Guiam")
+print("Date Created: Jily 06, 2023")
+print("Description: Python for ITE 306")
+
+print("")
+
+print("First Name: Ma. Mikcaela Ann")
+print("Middle Name: Evangelista")
+print("Last Name: Guiam")
+print("Course: Information Technology")
+print("Year: 3rd Year College")
+print("Gender: Female")
+print("Civil Status: Single")
+print("Nationality: Filipino")
+print("Motto: You are what you love not who loves you.")
+
+print("")
+
+print("PHINMA-Araullo University")
+print("CITE")
